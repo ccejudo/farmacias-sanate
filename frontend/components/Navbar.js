5acf2b7d713295d10fb206d8b93fcd0f1d0a5456
ccejudo <cristopher.cm1999@gmail.com>
2021-11-03 10:28:01 -0600

    Add Navbar design

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -1,11 +1,31 @@
 import React from 'react'
-import { Grid } from '@mui/material'
+import { Grid, Stack, Button } from '@mui/material'
 import Link from 'next/link'
+
+import styles from '../styles/navbar.module.css'
 
 export default function Navbar() {
     return (
-        <Grid container>
-            
-        </Grid> container
+        <Grid container className={styles.navbar}>
+            <Grid item xs={2}>
+                <img src="/logo/logo_white_large.png" alt="logo" style={{ width: '10rem' }}/>
+            </Grid>
+            <Grid item xs={10}>
+                <Stack spacing={4} direction="row" justifyContent="flex-end">
+                    <Link href="/">
+                        <a className={styles.animation}>Medicamentos</a>
+                    </Link>
+                    <Link href="/">
+                        <a className={styles.animation}>Suplementos</a>
+                    </Link>
+                    <Link href="/">
+                        <a className={styles.animation}>Cosméticos</a>
+                    </Link>
+                    <Link href="/">
+                        <a className={styles.animation}>Bebés</a>
+                    </Link>
+                </Stack>
+            </Grid>
+        </Grid>
     )
 }
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Navbar from '../components/Navbar'
 import '../styles/global.css'
 
 export default function App({ Component, pageProps }) {
@@ -7,6 +8,7 @@ export default function App({ Component, pageProps }) {
     <title>Farmacias Sanate</title>
     <link rel="icon" href="/favicon.ico" />
     </Head>
+    <Navbar />
     <Component {...pageProps} />
     </>
 }
