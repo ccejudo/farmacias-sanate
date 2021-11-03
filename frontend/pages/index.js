fb9a0bf876c1249170d4bb03445285ed4a1b0d7a
ccejudo <cristopher.cm1999@gmail.com>
2021-11-03 10:28:27 -0600

    Show promo image

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -6,7 +6,9 @@ import styles from '../styles/home.module.css'
 export default function Home() {
   return (
     <Grid container>
-
+      <Grid item xs={12}>
+        <img src="/images/promo.jpg" alt="promo"/>
+      </Grid>
     </Grid>
   )
 }
