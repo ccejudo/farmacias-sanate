630310bafe998b5151c1fd2567354d1b9be75a52
ccejudo <cristopher.cm1999@gmail.com>
2021-11-09 18:39:18 -0600

    Add category section

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -7,6 +7,9 @@ export default function App({ Component, pageProps }) {
     <Head>
     <title>Farmacias Sanate</title>
     <link rel="icon" href="/favicon.ico" />
+    <link rel="preconnect" href="https://fonts.googleapis.com"/> 
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/> 
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet"/>
     </Head>
     <Navbar />
     <Component {...pageProps} />
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -7,7 +7,38 @@ export default function Home() {
   return (
     <Grid container>
       <Grid item xs={12}>
-        <img src="/images/promo.jpg" alt="promo"/>
+        <img src='images/promo.jpg' alt='promo'/>
+      </Grid>
+      <Grid container item xs={12} className={styles.container_category}>
+        <Grid item xs={12}>
+          <h2>Categorías</h2>
+        </Grid>
+        <Grid container item xs={12} justifyContent='space-around'>
+          <Link href='/'>
+            <div className={styles.category_button}>
+              <img src='images/medicamentos.jpg' alt='medicamentos'/>
+              <h3 className={styles.back_purple}>Medicamentos</h3>
+            </div>
+          </Link>
+          <Link href='/'>
+            <div className={styles.category_button}>
+              <img src='images/suplementos.jpg' alt='suplementos'/>
+              <h3 className={styles.back_red}>Suplementos</h3>
+            </div>
+          </Link>
+          <Link href='/'>
+            <div className={styles.category_button}>
+              <img src='images/cosmeticos.jpg' alt='suplementos'/>
+              <h3 className={styles.back_green}>Cosméticos</h3>
+            </div>
+          </Link>
+          <Link href='/'>
+            <div className={styles.category_button}>
+              <img src='images/bebes.jpeg' alt='suplementos'/>
+              <h3 className={styles.back_blue}>Bebés</h3>
+            </div>
+          </Link>
+        </Grid>
       </Grid>
     </Grid>
   )
