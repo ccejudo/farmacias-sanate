b9b2966aeeb8e3aac9e6bb575f876315c1e1ac9c
ccejudo <cristopher.cm1999@gmail.com>
2021-11-22 13:12:55 -0600

    Add admin dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import Suplementos from './pages/Suplementos'
 import Bebes from './pages/Bebes'
 import Cosmeticos from './pages/Cosmeticos'
 import Profile from './pages/Profile'
+import AdminDashboard from "./pages/AdminDashboard";
 /*
 import Login from './pages/Login'
 import Signup from './pages/Signup'
@@ -17,6 +18,7 @@ function App() {
     <Router>
       <Switch>
         <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/inventory" element={<AdminDashboard/>}/>
         <Route exact path="/medicamentos" element={<Medicamentos/>}/> 
         <Route exact path="/suplementos" element={<Suplementos/>}/>
         <Route exact path="/bebes" element={<Bebes/>}/>
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -5,7 +5,8 @@ import { Link } from "react-router-dom"
 
 import styles from '../styles/navbar.module.css'
 
-export default function Navbar() {
+export default function Navbar( props ) {
+    const role = props.role || null
     return (
         <Grid container className={styles.navbar}>
             <Grid item xs={2}>
@@ -13,28 +14,30 @@ export default function Navbar() {
                     <img src="/logo/logo_white_large.png" alt="logo" style={{ width: '10rem' }}/>
                 </Link>
             </Grid>
-            <Grid item xs={10}>
-                <Stack spacing={4} direction="row" justifyContent="flex-end">
-                    <Link to="/">
-                        <a className={styles.animation}>Inicio</a>
-                    </Link>
-                    <Link to="/medicamentos">
-                        <a className={styles.animation}>Medicamentos</a>
-                    </Link>
-                    <Link to="/suplementos">
-                        <a className={styles.animation}>Suplementos</a>
-                    </Link>
-                    <Link to="/cosmeticos">
-                        <a className={styles.animation}>Cosméticos</a>
-                    </Link>
-                    <Link to="/bebes">
-                        <a className={styles.animation}>Bebés</a>
-                    </Link>
-                    <Link to="/profile">
-                        <Person fontSize="large"/>
-                    </Link>
-                </Stack>
-            </Grid>
+            { !role? <>
+                <Grid item xs={10}>
+                    <Stack spacing={4} direction="row" justifyContent="flex-end">
+                        <Link to="/">
+                            <a className={styles.animation}>Inicio</a>
+                        </Link>
+                        <Link to="/medicamentos">
+                            <a className={styles.animation}>Medicamentos</a>
+                        </Link>
+                        <Link to="/suplementos">
+                            <a className={styles.animation}>Suplementos</a>
+                        </Link>
+                        <Link to="/cosmeticos">
+                            <a className={styles.animation}>Cosméticos</a>
+                        </Link>
+                        <Link to="/bebes">
+                            <a className={styles.animation}>Bebés</a>
+                        </Link>
+                        <Link to="/profile">
+                            <Person fontSize="large"/>
+                        </Link>
+                    </Stack>
+                </Grid>
+            </> : <></> }
         </Grid>
     )
 }
