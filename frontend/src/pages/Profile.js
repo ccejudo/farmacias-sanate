98a41db16a51df663b6321ed38b6dc565d15e92a
ccejudo <cristopher.cm1999@gmail.com>
2021-11-22 13:13:58 -0600

    Add addresses to profile

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../components/Navbar'
-import { Grid } from '@mui/material'
+import { Grid, Paper } from '@mui/material'
 
 export default function Profile() {
+    const [name, setName] = useState('test')
     return (
         <Grid container>
             <Navbar />
-            <h1>Profile</h1>
+            <Grid container item xs={12} className="container_category">
+                <Grid item xs={12}>
+                    <h1>¡Bienvenido {name}!</h1>
+                </Grid>
+                <Grid container item xs={12}>
+                    <Paper elevation={4} style={{padding: '3%'}}>
+                        <h2>Dirección</h2>
+                        <p>Calle de la calle #123</p>
+                    </Paper>
+                </Grid>
+            </Grid>
         </Grid>
     )
 }
